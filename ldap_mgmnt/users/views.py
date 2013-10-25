ec539ea239b3f1b947a272a828184eb3c803ca7e
ldap_mgmnt <ldap_mgmnt@directoryone.jnu.ac.in>
2013-10-25 09:50:35 +0530

    Updated liscence declaration in all files

--- ldap_mgmnt/users/views.py
+++ ldap_mgmnt/users/views.py
@@ -1,1 +1,20 @@
+# 
+# (C) 2013 Varun Mittal <varunmittal91@gmail.com>
+# This program is distributed under the terms of the GNU General Public License v2
+#
+# This file is part of ldap-mgmnt.
+# 
+# ldap-mgmnt is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation version 3 of the License. 
+#
+# ldap-mgmnt is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NeweraHPC. If not, see <http://www.gnu.org/licenses/>.
+# 
+
 # Create your views here.
